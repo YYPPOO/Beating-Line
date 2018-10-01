6c3beff8170ae208eac0d93b74ef10137b840819
YYPPOO <kazjiay@gmail.com>
2018-10-01 22:12:37 +0800

    move files to public file

---
+++ public/js/firebaseInit.js
@@ -0,0 +1,9 @@
+let config = {
+    apiKey: "AIzaSyBP9wMIOZ5j8K5F39AJFrGJ-WpERHMUE3c",
+    authDomain: "beating-line.firebaseapp.com",
+    databaseURL: "https://beating-line.firebaseio.com",
+    projectId: "beating-line",
+    storageBucket: "beating-line.appspot.com",
+    messagingSenderId: "498561389182"
+};
+firebase.initializeApp(config);
