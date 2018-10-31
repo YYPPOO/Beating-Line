7152e54bfc84a4005f4ad6691b630fb83e25b478
YYPPOO <kazjiay@gmail.com>
2018-10-31 18:46:14 +0800

    add test, improve app speed

--- test/test.js
+++ test/test.js
@@ -1,8 +1,39 @@
-let assert = require('assert');
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal([1,2,3].indexOf(4), -1);
-        });
-    });
-});
+// let chai = require("chai");
+// let chaiAsPromised = require("chai-as-promised");
+// let expect = require('chai').expect;
+// chai.use(chaiAsPromised);
+
+describe('#app.stop', function() {
+    it('should return to initial state', function() {
+        // app.play();
+        state.p++;
+        app.stop();
+        chai.expect(state.playing).to.be.false;
+        chai.expect(state.p).to.be.equal(0);
+    })
+})
+describe('#recording', function() {
+    it('keyboard record to beat pad', function() {
+        app.play();
+        document.getElementById("drumPad1").click();
+        chai.expect(state.playing).to.be.true;
+        chai.expect(dom.padList[state.p][1]).to.be.true;
+    })
+})
+
+// describe('#stop', function(){
+//     it('should return to initial state', function() {
+//         app.stop();
+//         chai.assert.equal(state.playing, false);
+//         chai.assert.equal(state.p, 0);
+//     })
+// })
+
+// let assert = require('assert');
+// describe('Array', function() {
+//     describe('#indexOf()', function() {
+//         it('should return -1 when the value is not present', function() {
+//             assert.equal([1,2,3].indexOf(4), -1);
+//         });
+//     });
+// });
