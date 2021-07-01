335512f66cb2f7a9032214700d7e631f1d97b1f9
YYPPOO <kazjiay@gmail.com>
2021-07-01 16:56:10 +0800

    recharge indent

--- public/js/firebaseInit.js
+++ public/js/firebaseInit.js
@@ -1,9 +1,9 @@
 let config = {
-    apiKey: "AIzaSyBP9wMIOZ5j8K5F39AJFrGJ-WpERHMUE3c",
-    authDomain: "beating-line.firebaseapp.com",
-    databaseURL: "https://beating-line.firebaseio.com",
-    projectId: "beating-line",
-    storageBucket: "beating-line.appspot.com",
-    messagingSenderId: "498561389182"
+  apiKey: "AIzaSyBP9wMIOZ5j8K5F39AJFrGJ-WpERHMUE3c",
+  authDomain: "beating-line.firebaseapp.com",
+  databaseURL: "https://beating-line.firebaseio.com",
+  projectId: "beating-line",
+  storageBucket: "beating-line.appspot.com",
+  messagingSenderId: "498561389182"
 };
 let ourFirebase = firebase.initializeApp(config);
