a93f399ec67a786ae891eaf101715081578ef900
YYPPOO <kazjiay@gmail.com>
2021-07-01 21:49:15 +0800

    refactor coding style

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,10 +1,8 @@
-let dbHost = "https://beating-line.firebaseapp.com";
+const dbHost = "https://beating-line.firebaseapp.com";
 
-let authStatus = function () {
-  return firebase.auth().currentUser;
-}
+const authStatus = () => firebase.auth().currentUser;
 
-firebase.auth().onAuthStateChanged(function (user) {
+firebase.auth().onAuthStateChanged(user => {
   if (user) {
     console.log(user);
     showUserPic(user);
@@ -18,52 +16,51 @@ firebase.auth().onAuthStateChanged(function (user) {
 });
 
 // facebook log in --------------------------------------------
-let fbLogin = function () {
-  let provider = new firebase.auth.FacebookAuthProvider();
+const fbLogin = () => {
+  const provider = new firebase.auth.FacebookAuthProvider();
   firebase.auth().signInWithRedirect(provider);
   alert("Page is reloading...", true);
 };
 
 // google log in ----------------------------------------------
-let gLogin = function () {
-  let provider = new firebase.auth.GoogleAuthProvider();
+const gLogin = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithRedirect(provider);
   alert("Page is reloading...", true);
 }
 
-//confirm FB or google log in state, update back-end user database -------------------------
-let getRedirectResult = function () {
-  firebase.auth().getRedirectResult().then(function (result) {
-    if (result.user) {
-      let user = firebase.auth().currentUser;
-      let userData = {
+// confirm FB or google log in state, update back-end user database
+const getRedirectResult = () => {
+  firebase.auth().getRedirectResult()
+    .then(result => {
+      if (!(result && result.user)) throw new Error('fetch error');
+      const user = firebase.auth().currentUser;
+      const userData = {
         userName: user.displayName,
         userEmail: user.email,
         userId: user.uid,
         providerId: user.providerData[0].providerId
       };
-      fetch(dbHost + "/exe/manageAccount", {
+      return fetch(dbHost + "/exe/manageAccount", {
         method: "POST",
         body: JSON.stringify(userData),
         mode: 'cors',
         headers: {
           "Content-Type": "application/json"
         }
-      }).then(res => res.json())
-        .catch(error => console.error("Fetch error:", error))
-        .then(response => {
-          console.log("Log in success:", response);
-        });
-    }
-  }).catch(function (error) {
-    console.log(error);
-  });
+      })
+    })
+    .then(res => res.json())
+    .then(response => {
+      console.log("Log in success:", response);
+    })
+    .catch(error => console.error("Fetch error:", error));
 }
 
 // email password log in --------------------------------------------
-let signIn = function () {
-  let email = document.getElementById("logInEmail").value;
-  let password = document.getElementById("logInPassword").value;
+const signIn = () => {
+  const email = document.getElementById("logInEmail").value;
+  const password = document.getElementById("logInPassword").value;
   if (!/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(email)) {
     alert("Email error, please try again.");
     return;
@@ -74,28 +71,27 @@ let signIn = function () {
   }
   alert("Delivering the data.", true);
   firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(function (e) {
-      console.log(e);
-      alert("Log in success.", true)
+    .then(() => {
+      alert("Log in success.", true);
       document.location.reload();
     })
-    .catch(function (error) {
-      if (error.code == "auth/invalid-email" || error.code == "auth/user-not-found") {
-        alert("Email error, please try again.")
-      } else if (error.code == "auth/wrong-password") {
-        alert("Password error, please try again.")
-      } else {
-        alert(error.message, false);
-      }
+    .catch(error => {
       console.log(error);
+      if (error.code == "auth/invalid-email" || error.code == "auth/user-not-found") {
+        return alert("Email error, please try again.");
+      }
+      if (error.code == "auth/wrong-password") {
+        return alert("Password error, please try again.");
+      }
+      alert(error.message);
     });
 }
 
 // email password sign up ---------------------------------------------
-let signUp = function () {
-  let email = document.getElementById("logInEmail").value;
-  let password = document.getElementById("logInPassword").value;
-  let name = document.getElementById("logInName").value;
+const signUp = () => {
+  const email = document.getElementById("logInEmail").value;
+  const password = document.getElementById("logInPassword").value;
+  const name = document.getElementById("logInName").value;
   if (!name) {
     alert("Please enter your name.");
     return;
@@ -110,95 +106,98 @@ let signUp = function () {
   }
   alert("Delivering the data.", true);
   firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(function (e) {
-      let user = firebase.auth().currentUser;
+    .then(() => {
+      const user = firebase.auth().currentUser;
       user.updateProfile({ displayName: name })
-        .then(function () {
+        .then(() => {
           // Update successful.
-          user.sendEmailVerification().then(function () {
-            console.log(user);
-            console.log("Email sent.");
-            let userData = {
-              userName: user.displayName,
-              userEmail: user.email,
-              userId: user.uid,
-              providerId: user.providerData[0].providerId
-            };
-            fetch(dbHost + "/exe/manageAccount", {
-              method: "POST",
-              body: JSON.stringify(userData),
-              headers: {
-                "Content-Type": "application/json"
-              }
-            }).then(res => res.json())
-              .catch(error => console.error("Create account data error:", error))
-              .then(response => {
-                alert("Sign up success, and the verify email is send. Reload in 3 seconds.", true);
-                setTimeout(function () { document.location.reload() }, 3000);
+          user.sendEmailVerification()
+            .then(() => {
+              console.log(user);
+              console.log("Email sent.");
+              const userData = {
+                userName: user.displayName,
+                userEmail: user.email,
+                userId: user.uid,
+                providerId: user.providerData[0].providerId
+              };
+              fetch(dbHost + "/exe/manageAccount", {
+                method: "POST",
+                body: JSON.stringify(userData),
+                headers: {
+                  "Content-Type": "application/json"
+                }
               })
-          }).catch(function (error) {
-            console.log("send mail fail:" + error);
-          });
-        }).catch(function (error) {
+                .then(res => res.json())
+                .catch(error => console.error("Create account data error:", error))
+                .then(response => {
+                  alert("Sign up success, and the verify email is send. Reload in 3 seconds.", true);
+                  setTimeout(() => { document.location.reload() }, 3000);
+                })
+            })
+            .catch(error => {
+              console.log("send mail fail:" + error);
+            });
+        }).catch(error => {
           console.log("update profile fail:" + error);
         });
     })
-    .catch(function (error) {
-      if (error.code == "auth/weak-password") {
-        alert("The password is too weak, please try again.");
-      } else if (error.code == "auth/email-already-in-use") {
-        alert("This Email is already registered, please try again.")
-      } else {
-        alert(error.message);
-      }
+    .catch(error => {
       console.log(error);
+      if (error.code == "auth/weak-password") {
+        return alert("The password is too weak, please try again.");
+      }
+      if (error.code == "auth/email-already-in-use") {
+        return alert("This Email is already registered, please try again.")
+      }
+      alert(error.message);
     });
 }
 
-// ---------------------------------------------------
-let resetPassword = function () {
-  let email = document.getElementById("logInEmail").value;
+const resetPassword = () => {
+  const email = document.getElementById("logInEmail").value;
   if (!/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(email)) {
     alert("Please enter your email.");
     return;
   }
-  firebase.auth().sendPasswordResetEmail(email).then(function () {
-    alert("The reset password email is send. Reload in 3 seconds.", true)
-    console.log("Email sent!")
-    setTimeout(document.location.reload(), 3000);
-  }).catch(function (error) {
-    alert("Email error, please try again.");
-    console.log(error);
-  });
+  firebase.auth().sendPasswordResetEmail(email)
+    .then(() => {
+      alert("The reset password email is send. Reload in 3 seconds.", true)
+      console.log("Email sent!")
+      setTimeout(document.location.reload(), 3000);
+    })
+    .catch(error => {
+      alert("Email error, please try again.");
+      console.log(error);
+    });
 }
 
-function goToProfile() {
+const goToProfile = () => {
   saveBeatToLocalStorage();
   window.location = "/profile.html";
 }
 
-// --------------------------------------------------------
-function popUpLogIn() {
+const popUpLogIn = () => {
   saveBeatToLocalStorage();
   if (authStatus() !== null) {
     return;
   }
 
-  let closeLogIn = function () {
+  const closeLogIn = function () {
     mySheild.parentNode.removeChild(mySheild);
     myLogIn.parentNode.removeChild(myLogIn);
   }
 
-  let mySheild = cE("div", "logInShield");
+  const mySheild = cE("div", "logInShield");
   mySheild.addEventListener('click', closeLogIn);
 
-  let myLogIn = cE("div", "logIn");
-  let myLogInDiv = cE("div", "logInDiv");
-  let myLogoImg = cE("img", "logInImg", null, "src", "img/logo.svg");
+  const myLogIn = cE("div", "logIn");
+  const myLogInDiv = cE("div", "logInDiv");
+  const myLogoImg = cE("img", "logInImg", null, "src", "img/logo.svg");
 
-  let myNameInput = cE("input", "logInName", null, "placeholder", "Name");
-  let myEmailInput = cE("input", "logInEmail", null, "placeholder", "Email");
-  let myPasswordInput = cE("input", "logInPassword", null, "placeholder", "********");
+  const myNameInput = cE("input", "logInName", null, "placeholder", "Name");
+  const myEmailInput = cE("input", "logInEmail", null, "placeholder", "Email");
+  const myPasswordInput = cE("input", "logInPassword", null, "placeholder", "********");
 
   myNameInput.type = "text";
   myNameInput.id = "logInName";
@@ -207,9 +206,9 @@ function popUpLogIn() {
   myPasswordInput.type = "password";
   myPasswordInput.id = "logInPassword";
 
-  let mySignLink = cE("a", "logInText", "Already have an account? Sign in here.");
-  mySignLink.addEventListener("click", function () {
-    let logging = this.textContent == "Already have an account? Sign in here.";
+  const mySignLink = cE("a", "logInText", "Already have an account? Sign in here.");
+  mySignLink.addEventListener("click", () => {
+    const logging = this.textContent === "Already have an account? Sign in here.";
     //true = sign up to sign in, false = sign in to sign up
     mySignLink.textContent = logging ? "Don't have an account? Sign up here." : "Already have an account? Sign in here.";
     myForget.textContent = logging ? "Forget password?" : "";
@@ -219,28 +218,28 @@ function popUpLogIn() {
     mySignButton.addEventListener("click", logging ? signIn : signUp);
   })
 
-  let myForget = cE("a", "logInText", "", "id", "forgetPassword");
+  const myForget = cE("a", "logInText", "", "id", "forgetPassword");
   myForget.addEventListener("click", resetPassword);
 
-  let mySignButton = cE("button", "", "Sign Up");
+  const mySignButton = cE("button", "", "Sign Up");
   mySignButton.addEventListener("click", signUp);
 
-  let myPolicyDiv = cE("div", "policyDiv");
-  let myPolicyText = cE("span", "policyText");
+  const myPolicyDiv = cE("div", "policyDiv");
+  const myPolicyText = cE("span", "policyText");
   myPolicyText.innerHTML = "Agree our <a class='logInText' target='_blank' href='terms.html'>Terms</a> and <a class='logInText' target='_blank' href='privacy.html'>Privacy Policy</a> by clicking.";
   myPolicyDiv.append(myPolicyText);
 
-  let myHrDiv = cE("div", "divideLine");
-  let myLine = cE("span", "hrLine");
-  let myOr = cE("span", "logInOr", "or Sign in with community account.");
-  let myLineTwo = cE("span", "hrLine");
+  const myHrDiv = cE("div", "divideLine");
+  const myLine = cE("span", "hrLine");
+  const myOr = cE("span", "logInOr", "or Sign in with community account.");
+  const myLineTwo = cE("span", "hrLine");
 
   myHrDiv.append(myLine, myOr, myLineTwo);
 
-  let myAlertBtnDiv = cE("div", "alertBtnDiv");
-  let myFB = cE("img", "logInIcon", "", "src", "img/facebook.svg");
+  const myAlertBtnDiv = cE("div", "alertBtnDiv");
+  const myFB = cE("img", "logInIcon", "", "src", "img/facebook.svg");
   myFB.addEventListener("click", fbLogin);
-  let myG = cE("img", "logInIcon", "", "src", "img/google.svg");
+  const myG = cE("img", "logInIcon", "", "src", "img/google.svg");
   myG.addEventListener("click", gLogin);
 
   myAlertBtnDiv.append(myFB, myG);
@@ -251,43 +250,37 @@ function popUpLogIn() {
   document.body.append(mySheild, myLogIn);
 }
 
-function getUserBeatList(uid) {
-  fetch(dbHost + "/exe/getUserBeat?userId=" + uid, {
-    method: "GET"
-  }).then(res => {
-    console.log(res);
-    return res.json();
-  })
+const getUserBeatList = uid => {
+  fetch(dbHost + "/exe/getUserBeat?userId=" + uid, { method: "GET" })
+    .then(res => res.json())
     .then(response => {
-      console.log("Load user beat: ", response);
-      if (!response.error) {
-        showUserBeatList(response, uid);
-      }
+      if (!response.error) showUserBeatList(response, uid);
     })
     .catch(error => {
       console.error("Load user beat error: ", error);
-    })
+    });
 }
 
-function showUserBeatList(userBeatList, uid) {
-  let myBeatListBtn = cE("button", "beatListBtn", "My Beat ");
+const showUserBeatList = (userBeatList, uid) => {
+  const myBeatListBtn = cE("button", "beatListBtn", "My Beat ");
   myBeatListBtn.id = "myBeat";
-  myBeatListBtn.addEventListener("click", function () {
+  myBeatListBtn.addEventListener("click", () => {
     myBeatListBtn.classList.toggle("beatListBtnShow");
     myBeatListDiv.classList.toggle("beatListDivShow");
   })
   document.getElementById("sideNav").appendChild(myBeatListBtn);
-  let myBeatListDiv = cE("div", "beatListDiv");
+  const myBeatListDiv = cE("div", "beatListDiv");
   for (let i in userBeatList) {
-    let myBeatDiv = cE("div", "beatDiv");
-    let myBeat = cE("div", "beatName", userBeatList[i], "id", i);
-    myBeat.addEventListener("click", function () {
+    const myBeatDiv = cE("div", "beatDiv");
+    const myBeat = cE("div", "beatName", userBeatList[i], "id", i);
+    myBeat.addEventListener("click", () => {
       fetch(dbHost + "/exe/getBeat?id=" + i, {
         method: "GET",
         headers: new Headers({
           "Content-Type": "application/json"
         })
-      }).then(res => res.json())
+      })
+      .then(res => res.json())
         .then(response => {
           if (response.error) {
             console.error("Load beat error:", response.error)
@@ -311,9 +304,9 @@ function showUserBeatList(userBeatList, uid) {
           console.error("Load beat error:", error)
         });
     })
-    let myBeatDelete = cE("div", "deleteBeat", "X");
-    myBeatDelete.addEventListener("click", function () {
-      alert("Delete the beat?!", false, function () {
+    const myBeatDelete = cE("div", "deleteBeat", "X");
+    myBeatDelete.addEventListener("click", () => {
+      alert("Delete the beat?!", false, () => {
         deleteBeat(i, uid);
       });
     })
@@ -323,10 +316,11 @@ function showUserBeatList(userBeatList, uid) {
   document.getElementById("sideNav").appendChild(myBeatListDiv);
 }
 
-function deleteBeat(deleteBeatId, uid) {
+const deleteBeat = (deleteBeatId, uid) => {
   fetch(dbHost + "/exe/deleteBeat?beatId=" + deleteBeatId + "&userId=" + uid, {
     method: "delete"
-  }).then(res => res.json())
+  })
+  .then(res => res.json())
     .then(response => {
       console.log(response);
       alert("Beat delete.", true);
@@ -336,15 +330,15 @@ function deleteBeat(deleteBeatId, uid) {
         beat.beatId = "";
         window.history.replaceState(null, "", "index.html");
       }
-    }).catch((error) => {
+    }).catch(error => {
       console.log(error);
       alert("Beat delete fail, please try again.")
     })
 }
 
-function removeUserBeatList() {
-  let beatListBtn = document.querySelector(".beatListBtn");
+const removeUserBeatList = () => {
+  const beatListBtn = document.querySelector(".beatListBtn");
   beatListBtn && beatListBtn.parentNode.removeChild(beatListBtn);
-  let beatListDiv = document.querySelector(".beatListDiv");
+  const beatListDiv = document.querySelector(".beatListDiv");
   beatListDiv && beatListDiv.parentNode.removeChild(beatListDiv);
 }
--- public/js/lib.js
+++ public/js/lib.js
@@ -1,44 +1,43 @@
 const cE = (type, v0, v1, k2, v2) => {
-  let myElement = document.createElement(type);
+  const myElement = document.createElement(type);
   myElement.className = v0;
   myElement.textContent = v1 ? v1 : "";
   myElement[k2] = v2;
   return myElement;
 };
 
-let app = {};
-app.get = (id) => {
-  return document.getElementById(id);
-};
+const app = {};
+app.get = id => document.getElementById(id);
 app.event = (id, event, callback) => {
   document.getElementById(id).addEventListener(event, callback);
 };
 
-let showUserPic = function (data) {
-  let storage = firebase.storage();
-  storage.ref(authStatus().uid + '/main.jpg').getDownloadURL().then((url) => {
-    console.log(url);
-    document.querySelector(".memberIcon").src = url;
-  }).catch((req) => {
-    document.querySelector(".memberIcon").src = data.photoURL ? data.photoURL : "../img/member.svg"; //+width;
-  })
+const showUserPic = data => {
+  const storage = firebase.storage();
+  storage.ref(authStatus().uid + '/main.jpg').getDownloadURL()
+    .then(url => {
+      document.querySelector(".memberIcon").src = url;
+    })
+    .catch(req => {
+      document.querySelector(".memberIcon").src = data.photoURL ? data.photoURL : "../img/member.svg";
+    })
 };
 
-function alert(text, boolean, cb) {
+function alert(text, isSuccess, callback) {
   document.activeElement.blur();
-  let lastSheild = document.querySelector(".shield");
+  const lastSheild = document.querySelector(".shield");
   lastSheild && lastSheild.parentNode.removeChild(lastSheild);
-  let lastAlert = document.querySelector(".alert");
+  const lastAlert = document.querySelector(".alert");
   lastAlert && lastAlert.parentNode.removeChild(lastAlert);
 
-  let closeAlert = function () {
+  const closeAlert = function () {
     document.removeEventListener("keydown", handleAlertKeyPress);
     mySheild.parentNode.removeChild(mySheild);
     myAlert.parentNode.removeChild(myAlert);
   }
-  let handleAlertKeyPress = function (e) {
-    e.stopImmediatePropagation();
-    switch (e.keyCode) {
+  const handleAlertKeyPress = event => {
+    event.stopImmediatePropagation();
+    switch (event.keyCode) {
       case 32: //" "
         myAlertBtn.click();
         break;
@@ -51,21 +50,21 @@ function alert(text, boolean, cb) {
     }
   };
 
-  let mySheild = cE("div", "shield");
+  const mySheild = cE("div", "shield");
   mySheild.addEventListener('click', closeAlert)
-  let myAlert = cE("div", "alert");
-  let myAlertImg = cE("img", "alertImg", "", "src", boolean ? "img/checked.svg" : "img/warning.svg");
-  let myAlertText = cE('div', "alertText", text);
-  let myAlertBtnDiv = cE("div", "alertBtnDiv");
-  let myAlertBtn = cE('button', "alertBtn", "Okay");
+  const myAlert = cE("div", "alert");
+  const myAlertImg = cE("img", "alertImg", "", "src", isSuccess ? "img/checked.svg" : "img/warning.svg");
+  const myAlertText = cE('div', "alertText", text);
+  const myAlertBtnDiv = cE("div", "alertBtnDiv");
+  const myAlertBtn = cE('button', "alertBtn", "Okay");
   myAlertBtn.addEventListener('click', function () {
     closeAlert();
-    cb && cb();
+    typeof callback === 'function' && callback();
   });
   document.addEventListener("keydown", handleAlertKeyPress);
   myAlertBtnDiv.appendChild(myAlertBtn);
-  if (cb) {
-    let myAlertBtn2 = cE('button', "alertCancel", "Cancel");
+  if (typeof callback === 'function') {
+    const myAlertBtn2 = cE('button', "alertCancel", "Cancel");
     myAlertBtn2.addEventListener('click', closeAlert);
     myAlertBtnDiv.appendChild(myAlertBtn2);
   }
